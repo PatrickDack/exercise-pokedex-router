10c3cd6979dbbd59a5d7fd7a52a32f2ac5cfe18a
Patrick Dack <patrickdack89@gmail.com>
2021-07-06 01:29:06 -0300

    Exercicio 1 - Criada uma rota para a pokedex no diretorio /

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <div className="App">
+        <h1> Pokedex </h1>
+    <BrowserRouter>
+        <Route path="/" render={(props) => <Pokedex {...props} pokemons={pokemons} />}/>
+    </BrowserRouter>
     </div>
   );
 }
